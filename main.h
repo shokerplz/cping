e088743cf98876db50277c5e101009eb15f8db34
Aleksandr <alekorneev@ozon.ru>
2021-05-14 09:16:08 +0300

    Enable log and bugfix

--- main.h
+++ main.h
@@ -10,7 +10,7 @@ bool is_dnsname = false; 												// Boolean for check if dns name promted or
 bool valid_args = true;  												// Boolean for check if args are valid
 struct sockaddr_in sa;   												// Struct which holds ip address. 
 int sockfd;              												// File descriptor for socket
-int logfd;               												// File decriptor for log file
+FILE* logfd;               												// File struct for log file
 in_addr_t dst_addr;      												// Variable which holds IPv4 destination address
 char *dst_hostname;      												// Variable which holds hostname in string format
 char *dst_ip;            												// Variable which holds IPv4 in string format
@@ -20,8 +20,8 @@ bool verbose_output;     												// Boolean for verbose output { true => ver
 int getIP(char *dst_hostname, char *dst_ip);  										// Get ip from hostname;
 int validate_args(bool *is_dnsname, char **dst_hostname, char **dst_ip, int argc, char *argv[], bool *verbose_output);  // Validate arguments
 void diag(int error_code); 												// Diagnostics
-int create_log(int *log_fd); 												// Log creation
-int log(int logfd, char* log_str); 											// Log function
+int create_log(FILE *log_fd); 												// Log creation
+int log(FILE*logfd, char* log_str); 											// Log function
 int validateIP(char *ip); 												// IPv4 validation
 int send_request_icmp(in_addr_t dst_addr, int *sockfd); 								// Send ICMP request to IPv4 addr
 int get_reply(int sockfd); 												// Get reply from socket
